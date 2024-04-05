3c00a43010a58a6e03ab3cb4a2669ff569c14a77
Gulshan <shrigulshankumar@gmail.com>
2024-04-06 02:33:56 +0300

    Changed browser firefox to chrome

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -3,8 +3,8 @@ const { test, expect } = require('@playwright/test');
 
 test.describe('My Test Suite', () => {
   test('My Test Case', async ({}) => {
-  //const browser = await chromium.launch();
-  const browser = await firefox.launch();
+  const browser = await chromium.launch();
+  // const browser = await firefox.launch();
   const page = await browser.newPage();
   await authenticate(page);
   await browser.close();
